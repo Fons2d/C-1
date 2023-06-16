019aed7190a9153b60c16c0108a11a7ba070e720
Igor <igor.1998@inbox.ru>
2023-06-17 00:27:34 +0300

    Добавил способ решения Programm14

--- task 14/Program.cs
+++ task 14/Program.cs
@@ -4,6 +4,25 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
+// void PatrulQuater(int numberA, int numberB)
+// {
+// if (numberA == numberB * numberB || numberB == numberA * numberA)
+// {
+// System.Console.WriteLine("Да");
+// }
+// else
+// {
+// System.Console.WriteLine("Нет");
+// }
+// }
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+PatrulQuater(numberA, numberB);
+
+
 
 Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
