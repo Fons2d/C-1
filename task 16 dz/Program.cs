a18750def7415a0532f341550a5bb54bd7fcb6b5
Igor <igor.1998@inbox.ru>
2023-06-22 23:14:48 +0400

    Programm 18

--- task 16 dz/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number>99 ? number.ToString()[2] : "Третьй цирфы нет");
-Console.ReadKey(true);
---
+++ task 18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+void PrintQuater(int x, int y)
+{
+    if  (x > 0 && y > 0)
+    {
+    System.Console.WriteLine("Точка располагается в первой четверти");
+    }
+    else if  (x < 0 && y > 0)
+    {
+    System.Console.WriteLine("Точка располагается в четвертой четверти");
+    }
+    else if  (x < 0 && y < 0)
+    {
+    System.Console.WriteLine("Точка располагается в третей четверти");
+    }
+    else 
+    {
+    System.Console.WriteLine("Точка располагается во второй четверти");
+    }
+}
+
+System.Console.WriteLine("Введите координату X:");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+PrintQuater(x, y);
