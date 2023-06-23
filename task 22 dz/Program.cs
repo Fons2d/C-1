97d63cdd9d23f043a76951cbe6a4f9a12feab165
Igor <igor.1998@inbox.ru>
2023-06-23 22:23:31 +0400

    Programm22

---
+++ task 22 dz/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// System.Console.Write("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int partnum1 = 0;
+// int partnum2 = 0;
+// if (num > 9999 && num < 100000)
+// {
+// partnum1 = num / 10000;
+// int firstpart = num / 1000 % 10;
+// partnum1 = firstpart + partnum1;
+// partnum2 = num % 100;
+// int secondpart = partnum2 / 10;
+// partnum2 = partnum2 % 10;
+// partnum2 = secondpart + partnum2;
+// }
+// else
+// {
+//     System.Console.Write("Вы ввели непятизначное число");
+
+// }
+// if (partnum1 == partnum2)
+// {
+//     System.Console.Write($"Число {num} является палиндромом");
+// }
+// else
+// {
+//    Console.WriteLine($"Число {num} не является палиндромом");
+// }
+
+
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"число {number} является палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"число {number} не является палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {number} не является пятизначным");
+}
+
+
