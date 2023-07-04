18ac97b7f63c4b126b4104c61f4be31d15a9b3f8
Igor <igor.1998@inbox.ru>
2023-07-04 04:53:34 +0300

    Programm 26 and 27

---
+++ task 27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Multiplication(int num)
+{
+int multiplication = 1;
+for (int i = 1; i <= num; i++)
+{
+multiplication = multiplication  * i;
+}
+return multiplication;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма чисел от 1 до {number} = {Multiplication(number)}");
